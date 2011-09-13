93c2f1933538db95968ab3348e3115fb3e6845ff
Jörg Wendland <joerg@wendlandnet.de>
2011-09-13 18:11:53 +0200

    - add Spaceship preset - set default delay to 200msec

--- presets.js
+++ presets.js
@@ -1,21 +1,29 @@
 var GameOfLifePresets = { };
     
-GameOfLifePresets.blinker = {
+GameOfLifePresets.Blinker = {
     sizeX: 5,
     sizeY: 5,
     setup: [[1, 2], [2, 2], [3, 2]]
 };
 
 
-GameOfLifePresets.clock = {
+GameOfLifePresets.Clock = {
     sizeX: 6,
     sizeY: 6,
     setup: [[1, 2], [2, 2], [3, 1], [2, 4], [3, 3], [4, 3]]
 };
 
 
-GameOfLifePresets.glider = {
+GameOfLifePresets.Glider = {
     sizeX: 30,
     sizeY: 30,
     setup: [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2]]
+};
+
+
+GameOfLifePresets.LightWeightSpaceShip = {
+    sizeX: 30,
+    sizeY: 12,
+    setup: [[0, 5], [0, 7], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4],
+            [6, 5], [6, 6], [5, 7], [2, 8], [3, 8]]
 };
